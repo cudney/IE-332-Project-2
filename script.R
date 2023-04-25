c09447bfbd46036c3d095800a00b891c13112442
hrishabhn <hrishabhn@outlook.com>
2023-04-25 11:42:48 -0400

    Added 'all_images' variable, 'Image' class

--- script.R
+++ script.R
@@ -3,8 +3,63 @@
 # Load packages
 library(tidyverse)
 library(keras)
-library(tensorflow) 
+library(tensorflow)
 library(reticulate)
+library(R6)
+
+# initialize model
+model <- load_model_tf("./dandelion_model")
+
+# initialize vector of all test images
+all_images <- c()
+for (type in c("grass", "dandelions")) {
+    for (file in list.files(paste("./test_photos/", type, sep = ""))) {
+        all_images <- append(all_images, Image$new(type, file))
+    }
+}
+
+
+# Image class
+Image <- R6Class("Image",
+    public = list(
+        type = NULL,
+        file_name = NULL,
+        file_path = NULL,
+        img = NULL,
+
+        # constructor
+        initialize = function(type, file_name) {
+            self$type <- type
+            self$file_name <- file_name
+            self$file_path <- paste("./test_photos/", type, "/", file_name, sep = "")
+            self$img <- image_load(self$file_path)
+        },
+
+        # function to check if an image is a dandelion or grass
+        check = function() {
+            test_image <- image_load(self$file_path, target_size = c(224, 224))
+            x <- image_to_array(test_image)
+            x <- array_reshape(x, c(1, dim(x)))
+            x <- x / 255
+            pred <- model %>% predict(x)
+            print(pred)
+
+            print(paste("File Name:", self$file_name))
+            print(paste("Actual Type:", self$type))
+            if (pred[1, 1] < 0.50) {
+                print("Predicted Type: 'grass'")
+                print("With Confidence:")
+                print(pred[1, 2])
+            } else if (pred[1, 2] < 0.50) {
+                print("Predicted Type: dandelions")
+                print("With Confidence:")
+                print(pred[1, 1])
+            } else {
+                print("ERROR")
+            }
+        }
+    )
+)
 
 # function to reset test images
 reset_test_images <- function() {
@@ -22,37 +77,9 @@ reset_test_images <- function() {
     }
 }
 
-# initialize model
-model <- load_model_tf("./dandelion_model")
-
-# function to check if an image is a dandelion or grass
-check_image <- function(type, file_name) {
-    test_image <- image_load(paste("./test_photos/", type, "/", file_name, sep = ""), target_size = c(224, 224))
-    x <- image_to_array(test_image)
-    x <- array_reshape(x, c(1, dim(x)))
-    x <- x / 255
-    pred <- model %>% predict(x)
-    print(pred)
-
-    print(paste("File Name:", file_name))
-    print(paste("Actual Type:", type))
-    if (pred[1, 1] < 0.50) {
-        print("Predicted Type: 'grass'")
-        print("With Confidence:")
-        print(pred[1, 2])
-    } else if (pred[1, 2] < 0.50) {
-        print("Predicted Type: dandelions")
-        print("With Confidence:")
-        print(pred[1, 1])
-    } else {
-        print("ERROR")
-    }
-}
-
+# function to check test images
 check_test_images <- function() {
-    for (type in c("grass", "dandelions")) {
-        for (file in list.files(paste("./test_photos/", type, sep = ""))) {
-            check_image(type, file)
-        }
+    for (image in all_images) {
+        image$check()
     }
 }
